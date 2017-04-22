fe339db170dd88f011c9c244c7419a41e778fcaf
charleshamesse <charles.hamesse@gmail.com>
2017-04-22 12:59:58 +0200

    Fixed objective

--- P1.dev.jl
+++ P1.dev.jl
@@ -11,14 +11,17 @@ m = Model(solver = CbcSolver())
 # Variables
 @variable(m, y[1:instance.n], Bin) # (6)
 @variable(m, x[1:instance.n,1:instance.n], Bin) # (7)
+@variable(m, z)
 
 # Objective
 # TODO: define objective properly (max value of sums, not sum of sums)
 # draft: maximum(sum(instance.d[i][j] * x[i][j] for j=1:instance.n) for i=1:instance.n))
-@objective(m, Min, sum(sum(instance.d[i,j] * x[i,j] for j = 1:instance.n) for i = 1:instance.n))  # (1), (2)
+# working non-solution: sum(sum(instance.d[i,j] * x[i,j] for j = 1:instance.n) for i = 1:instance.n)
+@objective(m, Min, z)  # (1)
 
 # Constraints
 for i = 1:instance.n
+  @constraint(m, sum(instance.d[i, j] * x[i, j] for j = 1:instance.n) <= z) # (2)
   @constraint(m, sum(x[i,j] for j = 1:instance.n) == 1) # (3)
   for j = 1:instance.n
     @constraint(m, x[i,j] <= y[j]) # (4)
