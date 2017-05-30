0790e31fad91b3ea9e45ab2f07c9b4042c02f61c
Erica <eberghma@ulb.ac.be>
2017-05-30 21:13:27 +0200

    error mismatch

--- helpers/bestHeuristicRandom.jl
+++ helpers/bestHeuristicRandom.jl
@@ -1,12 +1,13 @@
 #Select the best solution out of 1000 random solutions
 function bestHeuristicRandom(instance::PCInstance)
     nb = 1000;
-    solution = Array{Array{UInt8}}(nb) 
+
+    solution = Array(UInt8,instance.n,nb)#Array{Array{UInt8}}(nb) 
     obj = zeros(UInt16,nb)
     
     for i = 1:nb
-        solution[i] = random_heuristic(instance);
-        obj[i] = obj_value(solution[i], instance);
+        solution[:,i] = random_heuristic(instance);
+        obj[i] = obj_value(solution[:,i], instance);
     end;
     
     println(maximum(obj));
