8a3717c130573c46f9b9e30a1be4cbf14fa88b3d
Erica <eberghma@ulb.ac.be>
2017-05-30 20:22:00 +0200

    started comparing heuristics

--- test.jl
+++ test.jl
@@ -1,9 +1,30 @@
-include("PCInstance.jl")
-include("read_instance.jl")
+using JuMP
+using Cbc
+
+include("helpers/PCInstance.jl")
+include("helpers/read_instance.jl")
+include("helpers/2approx.jl")
+include("helpers/print.jl")
+include("helpers/random.jl")
+include("helpers/obj_value.jl")
+include("helpers/bestHeuristicRandom.jl")
+include("helpers/bestHeuristicMix.jl")
+include("helpers/bestHeuristicTwoApprox.jl")
+
+
 instance = read_instance("instances/1.out")
 
-include("functions.jl")
+println("Random")
+tic()
+solution = bestHeuristicRandom(instance)
+toc()
 
-solution = random_heuristic(instance.p, instance.n)
+println("2-approx")
+tic()
+solution = bestHeuristicTwoApprox(instance)
+toc()
 
-print_solution(solution)
+println("Mix")
+tic()
+solution = bestHeuristicMix(instance)
+toc()
